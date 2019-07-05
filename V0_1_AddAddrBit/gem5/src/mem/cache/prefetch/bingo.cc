6461b2f1a99c122542f8e3e4274f505b7788df0e
petite2 <petite2@163.com>
2019-07-05 12:14:26 -0400

    Fix using namespace std

--- V0_1_AddAddrBit/gem5/src/mem/cache/prefetch/bingo.cc
+++ V0_1_AddAddrBit/gem5/src/mem/cache/prefetch/bingo.cc
@@ -50,6 +50,8 @@
 #include "debug/HWPrefetch.hh"
 #include "mem/cache/prefetch/bingo.hh"
 
+using namespace std;
+
 BingoPrefetcher::BingoPrefetcher(const BingoPrefetcherParams *p)
     : QueuedPrefetcher(p),
     pattern_len(p->pattern_len), filter_table(p->filter_table_size),
@@ -163,4 +165,238 @@ BingoPrefetcher*
 BingoPrefetcherParams::create()
 {
     return new BingoPrefetcher(this);
+}
+
+Table::Table(int width, int height) : width(width), height(height), cells(height, vector<string>(width)) {}
+
+void Table::set_row(int row, const vector<string> &data, int start_col = 0) {
+    assert(data.size() + start_col == this->width);
+    for (unsigned col = start_col; col < this->width; col += 1)
+        this->set_cell(row, col, data[col]);
+}
+
+void Table::set_col(int col, const vector<string> &data, int start_row = 0) {
+    assert(data.size() + start_row == this->height);
+    for (unsigned row = start_row; row < this->height; row += 1)
+        this->set_cell(row, col, data[row]);
+}
+
+void Table::set_cell(int row, int col, string data) {
+    assert(0 <= row && row < (int)this->height);
+    assert(0 <= col && col < (int)this->width);
+    this->cells[row][col] = data;
+}
+
+void Table::set_cell(int row, int col, double data) {
+    this->oss.str("");
+    this->oss << setw(11) << fixed << setprecision(8) << data;
+    this->set_cell(row, col, this->oss.str());
+}
+
+void Table::set_cell(int row, int col, int64_t data) {
+    this->oss.str("");
+    this->oss << setw(11) << std::left << data;
+    this->set_cell(row, col, this->oss.str());
+}
+
+void Table::set_cell(int row, int col, int data) { this->set_cell(row, col, (int64_t)data); }
+
+void Table::set_cell(int row, int col, uint64_t data) { this->set_cell(row, col, (int64_t)data); }
+
+string Table::to_string() {
+    vector<int> widths;
+    for (unsigned i = 0; i < this->width; i += 1) {
+        int max_width = 0;
+        for (unsigned j = 0; j < this->height; j += 1)
+            max_width = max(max_width, (int)this->cells[j][i].size());
+        widths.push_back(max_width + 2);
+    }
+    string out;
+    out += Table::top_line(widths);
+    out += this->data_row(0, widths);
+    for (unsigned i = 1; i < this->height; i += 1) {
+        out += Table::mid_line(widths);
+        out += this->data_row(i, widths);
+    }
+    out += Table::bot_line(widths);
+    return out;
+}
+
+string Table::data_row(int row, const vector<int> &widths) {
+    string out;
+    for (unsigned i = 0; i < this->width; i += 1) {
+        string data = this->cells[row][i];
+        data.resize(widths[i] - 2, ' ');
+        out += " | " + data;
+    }
+    out += " |\n";
+    return out;
+}
+
+static string Table::top_line(const vector<int> &widths) { return Table::line(widths, "┌", "┬", "┐"); }
+
+static string Table::mid_line(const vector<int> &widths) { return Table::line(widths, "├", "┼", "┤"); }
+
+static string Table::bot_line(const vector<int> &widths) { return Table::line(widths, "└", "┴", "┘"); }
+
+static string Table::line(const vector<int> &widths, string left, string mid, string right) {
+    string out = " " + left;
+    for (unsigned i = 0; i < widths.size(); i += 1) {
+        int w = widths[i];
+        for (int j = 0; j < w; j += 1)
+            out += "─";
+        if (i != widths.size() - 1)
+            out += mid;
+        else
+            out += right;
+    }
+    return out + "\n";
+}
+
+FilterTable::FilterTable(int size) : Super(size) { assert(__builtin_popcount(size) == 1); }
+
+FilterTable::Entry * FilterTable::find(uint64_t region_number) {
+    Entry *entry = Super::find(region_number);
+    if (!entry)
+        return nullptr;
+    this->set_mru(region_number);
+    return entry;
+}
+
+void FilterTable::insert(uint64_t region_number, uint64_t pc, int offset) {
+    assert(!this->find(region_number));
+    Super::insert(region_number, {pc, offset});
+    this->set_mru(region_number);
+}
+
+AccumulationTable::AccumulationTable(int size, int pattern_len) : Super(size), pattern_len(pattern_len) {
+    assert(__builtin_popcount(size) == 1);
+    assert(__builtin_popcount(pattern_len) == 1);
+}
+
+/**
+    * @return A return value of false means that the tag wasn't found in the table and true means success.
+    */
+bool AccumulationTable::set_pattern(uint64_t region_number, int offset) {
+    Entry *entry = Super::find(region_number);
+    if (!entry)
+        return false;
+    entry->data.pattern[offset] = true;
+    this->set_mru(region_number);
+    return true;
+}
+
+AccumulationTable::Entry AccumulationTable::insert(FilterTable::Entry &entry) {
+    assert(!this->find(entry.key));
+    vector<bool> pattern(this->pattern_len, false);
+    pattern[entry.data.offset] = true;
+    Entry old_entry = Super::insert(entry.key, {entry.data.pc, entry.data.offset, pattern});
+    this->set_mru(entry.key);
+    return old_entry;
+}
+
+PatternHistoryTable::PatternHistoryTable(
+    int size, int pattern_len, int min_addr_width, int max_addr_width, int pc_width, int num_ways = 16)
+    : Super(size, num_ways), pattern_len(pattern_len), min_addr_width(min_addr_width),
+        max_addr_width(max_addr_width), pc_width(pc_width) {
+    assert(this->pc_width >= 0);
+    assert(this->min_addr_width >= 0);
+    assert(this->max_addr_width >= 0);
+    assert(this->max_addr_width >= this->min_addr_width);
+    assert(this->pc_width + this->min_addr_width > 0);
+    assert(__builtin_popcount(pattern_len) == 1);
+    this->index_len = __builtin_ctz(this->num_sets);
+}
+
+/* address is actually block number */
+void PatternHistoryTable::insert(uint64_t pc, uint64_t address, vector<bool> pattern) {
+    assert((int)pattern.size() == this->pattern_len);
+    int offset = address % this->pattern_len;
+    pattern = my_rotate(pattern, -offset);
+    uint64_t key = this->build_key(pc, address);
+
+    Entry *entry = Super::find(key);
+    if (!entry) {
+        Super::insert(key, {vector<SC2>(this->pattern_len)});
+        entry = Super::find(key);
+    }
+    for (int i = 0; i < this->pattern_len; i += 1)
+        entry->data.pattern[i].input(pattern[i]);
+
+    this->set_mru(key);
+}
+
+/**
+    * @return An un-rotated pattern if match was found, otherwise an empty vector.
+    * Finds best match and in case of ties, uses the MRU entry.
+    */
+vector<bool> PatternHistoryTable::find(uint64_t pc, uint64_t address) {
+    uint64_t key = this->build_key(pc, address);
+    uint64_t index = key % this->num_sets;
+    uint64_t tag = key / this->num_sets;
+    auto &set = this->entries[index];
+    uint64_t min_tag_mask = (1 << (this->pc_width + this->min_addr_width - this->index_len)) - 1;
+    uint64_t max_tag_mask = (1 << (this->pc_width + this->max_addr_width - this->index_len)) - 1;
+    vector<vector<SC2>> min_matches;
+    vector<SC2> pattern;
+    for (int i = 0; i < this->num_ways; i += 1) {
+        if (!set[i].valid)
+            continue;
+        bool min_match = ((set[i].tag & min_tag_mask) == (tag & min_tag_mask));
+        bool max_match = ((set[i].tag & max_tag_mask) == (tag & max_tag_mask));
+        vector<SC2> &cur_pattern = set[i].data.pattern;
+        if (max_match) {
+            this->set_mru(set[i].key);
+            pattern = cur_pattern;
+            break;
+        }
+        if (min_match) {
+            min_matches.push_back(cur_pattern);
+        }
+    }
+
+    vector<bool> ret(this->pattern_len);
+    if (pattern.empty()) {
+        /* no max match was found, time for a vote! */
+        ret = this->vote(min_matches);
+    } else {
+        for (int i = 0; i < this->pattern_len; i += 1)
+            ret[i] = pattern[i].output();
+    }
+
+    int offset = address % this->pattern_len;
+    ret = my_rotate(ret, +offset);
+    return ret;
+}
+
+uint64_t PatternHistoryTable::build_key(uint64_t pc, uint64_t address) {
+    pc &= (1 << this->pc_width) - 1;            /* use [pc_width] bits from pc */
+    address &= (1 << this->max_addr_width) - 1; /* use [addr_width] bits from address */
+    uint64_t offset = address & ((1 << this->min_addr_width) - 1);
+    uint64_t base = (address >> this->min_addr_width);
+    /* base + pc + offset */
+    uint64_t key = (base << (this->pc_width + this->min_addr_width)) | (pc << this->min_addr_width) | offset;
+    /* CRC */
+    uint64_t tag = ((pc << this->min_addr_width) | offset);
+    do {
+        tag >>= this->index_len;
+        key ^= tag & ((1 << this->index_len) - 1);
+    } while (tag > 0);
+    return key;
+}
+
+vector<bool> PatternHistoryTable::vote(const vector<vector<SC2>> &x, float thresh = THRESH) {
+    int n = x.size();
+    vector<bool> ret(this->pattern_len, false);
+    for (int i = 0; i < n; i += 1)
+        assert((int)x[i].size() == this->pattern_len);
+    for (int i = 0; i < this->pattern_len; i += 1) {
+        int cnt = 0;
+        for (int j = 0; j < n; j += 1)
+            if (x[j][i].output())
+                cnt += 1;
+        if (1.0 * cnt / n >= thresh)
+            ret[i] = true;
+    }
+    return ret;
 }
--- V0_1_AddAddrBit/gem5/src/mem/cache/prefetch/bingo.hh
+++ V0_1_AddAddrBit/gem5/src/mem/cache/prefetch/bingo.hh
@@ -18,103 +18,43 @@
 #include <vector>
 
 class Table {
-    using namespace std;
   public:
-    Table(int width, int height) : width(width), height(height), cells(height, vector<string>(width)) {}
+    Table(int width, int height);
 
-    void set_row(int row, const vector<string> &data, int start_col = 0) {
-        assert(data.size() + start_col == this->width);
-        for (unsigned col = start_col; col < this->width; col += 1)
-            this->set_cell(row, col, data[col]);
-    }
+    void set_row(int row, const std::vector<std::string> &data, int start_col = 0);
 
-    void set_col(int col, const vector<string> &data, int start_row = 0) {
-        assert(data.size() + start_row == this->height);
-        for (unsigned row = start_row; row < this->height; row += 1)
-            this->set_cell(row, col, data[row]);
-    }
+    void set_col(int col, const std::vector<std::string> &data, int start_row = 0);
 
-    void set_cell(int row, int col, string data) {
-        assert(0 <= row && row < (int)this->height);
-        assert(0 <= col && col < (int)this->width);
-        this->cells[row][col] = data;
-    }
+    void set_cell(int row, int col, std::string data);
 
-    void set_cell(int row, int col, double data) {
-        this->oss.str("");
-        this->oss << setw(11) << fixed << setprecision(8) << data;
-        this->set_cell(row, col, this->oss.str());
-    }
+    void set_cell(int row, int col, double data);
 
-    void set_cell(int row, int col, int64_t data) {
-        this->oss.str("");
-        this->oss << setw(11) << std::left << data;
-        this->set_cell(row, col, this->oss.str());
-    }
+    void set_cell(int row, int col, int64_t data);
 
-    void set_cell(int row, int col, int data) { this->set_cell(row, col, (int64_t)data); }
+    void set_cell(int row, int col, int data);
 
-    void set_cell(int row, int col, uint64_t data) { this->set_cell(row, col, (int64_t)data); }
+    void set_cell(int row, int col, uint64_t data);
 
-    string to_string() {
-        vector<int> widths;
-        for (unsigned i = 0; i < this->width; i += 1) {
-            int max_width = 0;
-            for (unsigned j = 0; j < this->height; j += 1)
-                max_width = max(max_width, (int)this->cells[j][i].size());
-            widths.push_back(max_width + 2);
-        }
-        string out;
-        out += Table::top_line(widths);
-        out += this->data_row(0, widths);
-        for (unsigned i = 1; i < this->height; i += 1) {
-            out += Table::mid_line(widths);
-            out += this->data_row(i, widths);
-        }
-        out += Table::bot_line(widths);
-        return out;
-    }
+    std::string to_string();
 
-    string data_row(int row, const vector<int> &widths) {
-        string out;
-        for (unsigned i = 0; i < this->width; i += 1) {
-            string data = this->cells[row][i];
-            data.resize(widths[i] - 2, ' ');
-            out += " | " + data;
-        }
-        out += " |\n";
-        return out;
-    }
+    std::string data_row(int row, const std::vector<int> &widths);
 
-    static string top_line(const vector<int> &widths) { return Table::line(widths, "┌", "┬", "┐"); }
+    static std::string top_line(const std::vector<int> &widths);
 
-    static string mid_line(const vector<int> &widths) { return Table::line(widths, "├", "┼", "┤"); }
+    static std::string mid_line(const std::vector<int> &widths);
 
-    static string bot_line(const vector<int> &widths) { return Table::line(widths, "└", "┴", "┘"); }
+    static std::string bot_line(const std::vector<int> &widths);
 
-    static string line(const vector<int> &widths, string left, string mid, string right) {
-        string out = " " + left;
-        for (unsigned i = 0; i < widths.size(); i += 1) {
-            int w = widths[i];
-            for (int j = 0; j < w; j += 1)
-                out += "─";
-            if (i != widths.size() - 1)
-                out += mid;
-            else
-                out += right;
-        }
-        return out + "\n";
-    }
+    static std::string line(const std::vector<int> &widths, std::string left, std::string mid, std::string right);
 
   private:
     unsigned width;
     unsigned height;
-    vector<vector<string>> cells;
-    ostringstream oss;
+    std::vector<std::vector<std::string>> cells;
+    std::ostringstream oss;
 };
 
 template <class T> class InfiniteCache {
-    using namespace std;
   public:
     class Entry {
       public:
@@ -162,7 +102,7 @@ template <class T> class InfiniteCache {
     /**
      * For debugging purposes.
      */
-    string log(vector<string> headers, function<void(Entry &, Table &, int)> write_data) {
+    std::string log(std::vector<std::string> headers, std::function<void(Entry &, Table &, int)> write_data) {
         Table table(headers.size(), entries.size() + 1);
         table.set_row(0, headers);
         unsigned i = 0;
@@ -175,12 +115,11 @@ template <class T> class InfiniteCache {
 
   protected:
     Entry last_erased_entry;
-    unordered_map<uint64_t, Entry> entries;
+    std::unordered_map<uint64_t, Entry> entries;
     int debug_level = 0;
 };
 
 template <class T> class SetAssociativeCache {
-    using namespace std;
   public:
     class Entry {
       public:
@@ -192,7 +131,7 @@ template <class T> class SetAssociativeCache {
     };
 
     SetAssociativeCache(int size, int num_ways)
-        : size(size), num_ways(num_ways), num_sets(size / num_ways), entries(num_sets, vector<Entry>(num_ways)),
+        : size(size), num_ways(num_ways), num_sets(size / num_ways), entries(num_sets, std::vector<Entry>(num_ways)),
           cams(num_sets) {
         assert(size % num_ways == 0);
         for (int i = 0; i < num_sets; i += 1)
@@ -224,7 +163,7 @@ template <class T> class SetAssociativeCache {
         }
         uint64_t index = key % this->num_sets;
         uint64_t tag = key / this->num_sets;
-        vector<Entry> &set = this->entries[index];
+        std::vector<Entry> &set = this->entries[index];
         int victim_way = -1;
         for (int i = 0; i < this->num_ways; i += 1)
             if (!set[i].valid) {
@@ -261,8 +200,8 @@ template <class T> class SetAssociativeCache {
     /**
      * For debugging purposes.
      */
-    string log(vector<string> headers, function<void(Entry &, Table &, int)> write_data) {
-        vector<Entry> valid_entries = this->get_valid_entries();
+    std::string log(std::vector<std::string> headers, std::function<void(Entry &, Table &, int)> write_data) {
+        std::vector<Entry> valid_entries = this->get_valid_entries();
         Table table(headers.size(), valid_entries.size() + 1);
         table.set_row(0, headers);
         for (unsigned i = 0; i < valid_entries.size(); i += 1)
@@ -281,8 +220,8 @@ template <class T> class SetAssociativeCache {
         return rand() % this->num_ways;
     }
 
-    vector<Entry> get_valid_entries() {
-        vector<Entry> valid_entries;
+    std::vector<Entry> get_valid_entries() {
+        std::vector<Entry> valid_entries;
         for (int i = 0; i < num_sets; i += 1)
             for (int j = 0; j < num_ways; j += 1)
                 if (entries[i][j].valid)
@@ -293,18 +232,17 @@ template <class T> class SetAssociativeCache {
     int size;
     int num_ways;
     int num_sets;
-    vector<vector<Entry>> entries;
-    vector<unordered_map<uint64_t, int>> cams;
+    std::vector<std::vector<Entry>> entries;
+    std::vector<std::unordered_map<uint64_t, int>> cams;
     int debug_level = 0;
 };
 
 template <class T> class LRUSetAssociativeCache : public SetAssociativeCache<T> {
-    using namespace std;
     typedef SetAssociativeCache<T> Super;
 
   public:
     LRUSetAssociativeCache(int size, int num_ways)
-        : Super(size, num_ways), lru(this->num_sets, vector<uint64_t>(num_ways)) {}
+        : Super(size, num_ways), lru(this->num_sets, std::vector<uint64_t>(num_ways)) {}
 
     void set_mru(uint64_t key) { *this->get_lru(key) = this->t++; }
 
@@ -313,7 +251,7 @@ template <class T> class LRUSetAssociativeCache : public SetAssociativeCache<T> 
   protected:
     /* @override */
     int select_victim(uint64_t index) {
-        vector<uint64_t> &lru_set = this->lru[index];
+        std::vector<uint64_t> &lru_set = this->lru[index];
         return min_element(lru_set.begin(), lru_set.end()) - lru_set.begin();
     }
 
@@ -324,12 +262,11 @@ template <class T> class LRUSetAssociativeCache : public SetAssociativeCache<T> 
         return &this->lru[index][way];
     }
 
-    vector<vector<uint64_t>> lru;
+    std::vector<std::vector<uint64_t>> lru;
     uint64_t t = 1;
 };
 
 template <class T> class NMRUSetAssociativeCache : public SetAssociativeCache<T> {
-    using namespace std;
     typedef SetAssociativeCache<T> Super;
 
   public:
@@ -351,11 +288,10 @@ template <class T> class NMRUSetAssociativeCache : public SetAssociativeCache<T>
         return way;
     }
 
-    vector<int> mru;
+    std::vector<int> mru;
 };
 
 template <class T> class LRUFullyAssociativeCache : public LRUSetAssociativeCache<T> {
-    using namespace std;
     typedef LRUSetAssociativeCache<T> Super;
 
   public:
@@ -363,7 +299,6 @@ template <class T> class LRUFullyAssociativeCache : public LRUSetAssociativeCach
 };
 
 template <class T> class NMRUFullyAssociativeCache : public NMRUSetAssociativeCache<T> {
-    using namespace std;
     typedef NMRUSetAssociativeCache<T> Super;
 
   public:
@@ -371,7 +306,6 @@ template <class T> class NMRUFullyAssociativeCache : public NMRUSetAssociativeCa
 };
 
 template <class T> class DirectMappedCache : public SetAssociativeCache<T> {
-    using namespace std;
     typedef SetAssociativeCache<T> Super;
 
   public:
@@ -381,72 +315,41 @@ template <class T> class DirectMappedCache : public SetAssociativeCache<T> {
 /** End Of Cache Framework **/
 
 class FilterTableData {
-    using namespace std;
   public:
     uint64_t pc;
     int offset;
 };
 
 class FilterTable : public LRUFullyAssociativeCache<FilterTableData> {
-    using namespace std;
     typedef LRUFullyAssociativeCache<FilterTableData> Super;
 
   public:
-    FilterTable(int size) : Super(size) { assert(__builtin_popcount(size) == 1); }
+    FilterTable(int size);
 
-    Entry *find(uint64_t region_number) {
-        Entry *entry = Super::find(region_number);
-        if (!entry)
-            return nullptr;
-        this->set_mru(region_number);
-        return entry;
-    }
+    Entry *find(uint64_t region_number);
 
-    void insert(uint64_t region_number, uint64_t pc, int offset) {
-        assert(!this->find(region_number));
-        Super::insert(region_number, {pc, offset});
-        this->set_mru(region_number);
-    }
+    void insert(uint64_t region_number, uint64_t pc, int offset);
 };
 
 class AccumulationTableData {
-    using namespace std;
   public:
     uint64_t pc;
     int offset;
-    vector<bool> pattern;
+    std::vector<bool> pattern;
 };
 
 class AccumulationTable : public LRUFullyAssociativeCache<AccumulationTableData> {
-    using namespace std;
     typedef LRUFullyAssociativeCache<AccumulationTableData> Super;
 
   public:
-    AccumulationTable(int size, int pattern_len) : Super(size), pattern_len(pattern_len) {
-        assert(__builtin_popcount(size) == 1);
-        assert(__builtin_popcount(pattern_len) == 1);
-    }
+    AccumulationTable(int size, int pattern_len);
 
     /**
      * @return A return value of false means that the tag wasn't found in the table and true means success.
      */
-    bool set_pattern(uint64_t region_number, int offset) {
-        Entry *entry = Super::find(region_number);
-        if (!entry)
-            return false;
-        entry->data.pattern[offset] = true;
-        this->set_mru(region_number);
-        return true;
-    }
+    bool set_pattern(uint64_t region_number, int offset);
 
-    Entry insert(FilterTable::Entry &entry) {
-        assert(!this->find(entry.key));
-        vector<bool> pattern(this->pattern_len, false);
-        pattern[entry.data.offset] = true;
-        Entry old_entry = Super::insert(entry.key, {entry.data.pc, entry.data.offset, pattern});
-        this->set_mru(entry.key);
-        return old_entry;
-    }
+    Entry insert(FilterTable::Entry &entry);
 
   private:
     int pattern_len;
@@ -464,122 +367,30 @@ template <class T> std::vector<T> my_rotate(const std::vector<T> &x, int n) {
 #define THRESH 0.20
 
 class PatternHistoryTableData {
-    using namespace std;
   public:
-    vector<SC2> pattern;
+    std::vector<SC2> pattern;
 };
 
 class PatternHistoryTable : LRUSetAssociativeCache<PatternHistoryTableData> {
-    using namespace std;
     typedef LRUSetAssociativeCache<PatternHistoryTableData> Super;
 
   public:
     PatternHistoryTable(
-        int size, int pattern_len, int min_addr_width, int max_addr_width, int pc_width, int num_ways = 16)
-        : Super(size, num_ways), pattern_len(pattern_len), min_addr_width(min_addr_width),
-          max_addr_width(max_addr_width), pc_width(pc_width) {
-        assert(this->pc_width >= 0);
-        assert(this->min_addr_width >= 0);
-        assert(this->max_addr_width >= 0);
-        assert(this->max_addr_width >= this->min_addr_width);
-        assert(this->pc_width + this->min_addr_width > 0);
-        assert(__builtin_popcount(pattern_len) == 1);
-        this->index_len = __builtin_ctz(this->num_sets);
-    }
+        int size, int pattern_len, int min_addr_width, int max_addr_width, int pc_width, int num_ways = 16);
 
     /* address is actually block number */
-    void insert(uint64_t pc, uint64_t address, vector<bool> pattern) {
-        assert((int)pattern.size() == this->pattern_len);
-        int offset = address % this->pattern_len;
-        pattern = my_rotate(pattern, -offset);
-        uint64_t key = this->build_key(pc, address);
-
-        Entry *entry = Super::find(key);
-        if (!entry) {
-            Super::insert(key, {vector<SC2>(this->pattern_len)});
-            entry = Super::find(key);
-        }
-        for (int i = 0; i < this->pattern_len; i += 1)
-            entry->data.pattern[i].input(pattern[i]);
-
-        this->set_mru(key);
-    }
+    void insert(uint64_t pc, uint64_t address, std::vector<bool> pattern);
 
     /**
      * @return An un-rotated pattern if match was found, otherwise an empty vector.
      * Finds best match and in case of ties, uses the MRU entry.
      */
-    vector<bool> find(uint64_t pc, uint64_t address) {
-        uint64_t key = this->build_key(pc, address);
-        uint64_t index = key % this->num_sets;
-        uint64_t tag = key / this->num_sets;
-        auto &set = this->entries[index];
-        uint64_t min_tag_mask = (1 << (this->pc_width + this->min_addr_width - this->index_len)) - 1;
-        uint64_t max_tag_mask = (1 << (this->pc_width + this->max_addr_width - this->index_len)) - 1;
-        vector<vector<SC2>> min_matches;
-        vector<SC2> pattern;
-        for (int i = 0; i < this->num_ways; i += 1) {
-            if (!set[i].valid)
-                continue;
-            bool min_match = ((set[i].tag & min_tag_mask) == (tag & min_tag_mask));
-            bool max_match = ((set[i].tag & max_tag_mask) == (tag & max_tag_mask));
-            vector<SC2> &cur_pattern = set[i].data.pattern;
-            if (max_match) {
-                this->set_mru(set[i].key);
-                pattern = cur_pattern;
-                break;
-            }
-            if (min_match) {
-                min_matches.push_back(cur_pattern);
-            }
-        }
-
-        vector<bool> ret(this->pattern_len);
-        if (pattern.empty()) {
-            /* no max match was found, time for a vote! */
-            ret = this->vote(min_matches);
-        } else {
-            for (int i = 0; i < this->pattern_len; i += 1)
-                ret[i] = pattern[i].output();
-        }
-
-        int offset = address % this->pattern_len;
-        ret = my_rotate(ret, +offset);
-        return ret;
-    }
+    std::vector<bool> find(uint64_t pc, uint64_t address);
 
   private:
-    uint64_t build_key(uint64_t pc, uint64_t address) {
-        pc &= (1 << this->pc_width) - 1;            /* use [pc_width] bits from pc */
-        address &= (1 << this->max_addr_width) - 1; /* use [addr_width] bits from address */
-        uint64_t offset = address & ((1 << this->min_addr_width) - 1);
-        uint64_t base = (address >> this->min_addr_width);
-        /* base + pc + offset */
-        uint64_t key = (base << (this->pc_width + this->min_addr_width)) | (pc << this->min_addr_width) | offset;
-        /* CRC */
-        uint64_t tag = ((pc << this->min_addr_width) | offset);
-        do {
-            tag >>= this->index_len;
-            key ^= tag & ((1 << this->index_len) - 1);
-        } while (tag > 0);
-        return key;
-    }
+    uint64_t build_key(uint64_t pc, uint64_t address);
 
-    vector<bool> vote(const vector<vector<SC2>> &x, float thresh = THRESH) {
-        int n = x.size();
-        vector<bool> ret(this->pattern_len, false);
-        for (int i = 0; i < n; i += 1)
-            assert((int)x[i].size() == this->pattern_len);
-        for (int i = 0; i < this->pattern_len; i += 1) {
-            int cnt = 0;
-            for (int j = 0; j < n; j += 1)
-                if (x[j][i].output())
-                    cnt += 1;
-            if (1.0 * cnt / n >= thresh)
-                ret[i] = true;
-        }
-        return ret;
-    }
+    std::vector<bool> vote(const std::vector<std::vector<SC2>> &x, float thresh = THRESH);
 
     int pattern_len, index_len;
     int min_addr_width, max_addr_width, pc_width;
@@ -588,7 +399,6 @@ class PatternHistoryTable : LRUSetAssociativeCache<PatternHistoryTableData> {
 
 class BingoPrefetcher : public QueuedPrefetcher
 {
-    using namespace std;
   private:
     int pattern_len;
     FilterTable filter_table;
@@ -597,7 +407,7 @@ class BingoPrefetcher : public QueuedPrefetcher
     int debug_level = 0;
     const bool useMasterId;
 
-    vector<bool> find_in_pht(uint64_t pc, uint64_t address);
+    std::vector<bool> find_in_pht(uint64_t pc, uint64_t address);
     void insert_in_pht(const AccumulationTable::Entry &entry);
 
   public:
